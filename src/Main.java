3058576852128a72d3029e44b2eeb65ebefe8afd
Behruzbek <behruznosirjonov23@gmail.com>
2023-10-29 21:30:01 +0300

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-        
+        System.out.println("dawdaawd");
+        System.out.println("ddawdawdw");
+        System.out.println("ddawdawdw");
+        System.out.println("ddawdawdw");
+        System.out.println("ddawdawdw");
+
     }
 }
