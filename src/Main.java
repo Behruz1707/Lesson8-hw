5d851bd69bee162fd4c628b0aa9f806c8f0195b2
Behruzbek <behruznosirjonov23@gmail.com>
2023-10-29 21:31:26 +0300

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dawdaawd");
-        System.out.println("ddawdawdw");
-        System.out.println("ddawdawdw");
-        System.out.println("ddawdawdw");
-        System.out.println("ddawdawdw");
+
 
     }
 }
